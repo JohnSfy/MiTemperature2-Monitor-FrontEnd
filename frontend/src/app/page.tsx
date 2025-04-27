17e17d425e556c120ec1ca98765cc32569968392
JohnSfy <you@example.com>
2025-04-27 19:55:40 +0300

    changed web Service

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -21,10 +21,7 @@ export default function Page() {
 
   useEffect(() => {
     async function fetchData() {
-      // const response = await fetch("https://crucial-lelah-tsigkani2-80186950.koyeb.app/sensors")
-      const response = await fetch("https://mitemperature2-webservice.onrender.com/sensors")
-       //const response = await fetch('http://192.168.1.5:8000/sensors')
-
+      const response = await fetch('https://lazarosadamos.site/sensors');
       const data = await response.json()
       setSensors(data)
 
